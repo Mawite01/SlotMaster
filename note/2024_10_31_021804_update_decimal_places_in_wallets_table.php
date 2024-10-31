6add83a558bd1e3f0feb9f2133d4918c2d63fc36
AMK1215 <aungmyokhaing1215@gmail.com>
2024-10-31 09:04:37 +0630

    wallet update

---
+++ note/2024_10_31_021804_update_decimal_places_in_wallets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('wallets', function (Blueprint $table) {
+             //$table->unsignedSmallInteger('decimal_places')->default(4)->change();
+             DB::statement("ALTER TABLE wallets MODIFY decimal_places SMALLINT UNSIGNED DEFAULT 4");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('wallets', function (Blueprint $table) {
+             $table->unsignedSmallInteger('decimal_places')->default(2)->change();
+        });
+    }
+};
