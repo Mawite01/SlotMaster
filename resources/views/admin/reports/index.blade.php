7ef829d8864a3318b5b54cd62746944060c763b8
AMK1215 <aungmyokhaing1215@gmail.com>
2024-11-09 07:49:33 +0630

    CancelBetNResult api call updated by amk

--- resources/views/admin/reports/index.blade.php
+++ resources/views/admin/reports/index.blade.php
@@ -6,7 +6,7 @@
                 <div class="col-12">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
-                        <li class="breadcrumb-item active">Player Lists</li>
+                        <li class="breadcrumb-item active">W/L Report</li>
                     </ol>
                 </div>
             </div>
@@ -19,8 +19,8 @@
             <div class="row">
                 <div class="col-12">
                     <div class="d-flex justify-content-end mb-3">
-                        <a href="{{ route('admin.player.create') }}" class="btn btn-success " style="width: 100px;"><i
-                                class="fas fa-plus text-white  mr-2"></i>Create</a>
+                        <a href="{{ route('home') }}" class="btn btn-success " style="width: 100px;"><i
+                                class="fas fa-plus text-white  mr-2"></i>Back</a>
                     </div>
                     <div class="card " style="border-radius: 20px;">
                         <div class="card-header">
@@ -30,23 +30,23 @@
                             <table id="mytable" class="table table-bordered table-hover">
                                 <thead>
                                     <tr>
-                                        <th>Game Provider Name</th>
                                         <th>User Name</th>
+                                        <th>Game Provider Name</th>
+                                        <th>Total Stake</th>
                                         <th>Total Bet Amount</th>
                                         <th>Total Win Amount</th>
                                         <th>Total Net Win</th>
-                                        <th>Total Games</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     @foreach ($report as $row)
                                         <tr>
-                                            <td>{{ $row->game_provide_name }}</td>
                                             <td>{{ $row->user_name }}</td>
+                                            <td>{{ $row->game_provide_name }}</td>
+                                            <td>{{ $row->total_games }}</td>
                                             <td>{{ number_format($row->total_bet_amount, 2) }}</td>
                                             <td>{{ number_format($row->total_win_amount, 2) }}</td>
                                             <td>{{ number_format($row->total_net_win, 2) }}</td>
-                                            <td>{{ $row->total_games }}</td>
                                         </tr>
                                     @endforeach
                                 </tbody>
