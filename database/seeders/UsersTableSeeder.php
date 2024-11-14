609169ebcfe09a307e331ca5741765c9c710782c
AMK1215 <aungmyokhaing1215@gmail.com>
2024-11-14 08:33:40 +0630

    SystemWallet updated by amk

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -28,6 +28,15 @@ class UsersTableSeeder extends Seeder
 
         $systemWallet = $this->createUser(UserType::SystemWallet, 'SystemWallet', 'systemWallet', '09222222222');
         (new WalletService)->deposit($systemWallet, 50 * 100_0000, TransactionName::CapitalDeposit);
+
+        $player1 = $this->createUser(UserType::Player, 'Player2', 'Player002', '09111111112', $agent->id);
+        (new WalletService)->transfer($agent, $player1, 30000.1554, TransactionName::CreditTransfer);
+        $player2 = $this->createUser(UserType::Player, 'Player3', 'Player003', '09111111113', $agent->id);
+        (new WalletService)->transfer($agent, $player2, 30000.1554, TransactionName::CreditTransfer);
+        $player3 = $this->createUser(UserType::Player, 'Player3', 'Player003', '09111111114', $agent->id);
+        (new WalletService)->transfer($agent, $player3, 30000.1554, TransactionName::CreditTransfer);
+        $player4 = $this->createUser(UserType::Player, 'Player4', 'Player004', '09111111115', $agent->id);
+        (new WalletService)->transfer($agent, $player4, 30000.1554, TransactionName::CreditTransfer);
     }
 
     private function createUser(UserType $type, $name, $user_name, $phone, $parent_id = null, $referral_code = null)
