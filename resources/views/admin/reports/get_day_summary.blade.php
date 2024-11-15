2b4348d13b5a707591ec4d82081ddbcc0a7987d8
AMK1215 <aungmyokhaing1215@gmail.com>
2024-11-15 08:46:49 +0630

    get day summary updated amk

--- resources/views/admin/reports/get_day_summary.blade.php
+++ resources/views/admin/reports/get_day_summary.blade.php
@@ -19,48 +19,58 @@
             <div class="row">
                 <div class="col-12">
                     <div class="d-flex justify-content-end mb-3">
-                        <a href="{{ route('home') }}" class="btn btn-success " style="width: 100px;"><i
-                                class="fas fa-plus text-white  mr-2"></i>Back</a>
+                        <a href="{{ route('home') }}" class="btn btn-success" style="width: 100px;">
+                            <i class="fas fa-plus text-white mr-2"></i>Back
+                        </a>
                     </div>
-                    <div class="card " style="border-radius: 20px;">
+                    <div class="card" style="border-radius: 20px;">
                         <div class="card-header">
                             <h3>Get Day Summary</h3>
                         </div>
-
-
-
                         <div class="card-body">
                             <form action="{{ route('daily_summary') }}" method="POST">
-
                                 @csrf
-                                <label for="OperatorId">OperatorId:</label>
-                                <input type="text" id="OperatorId" name="OperatorId" value="delightMMK" required><br><br>
+                                <div class="form-row">
+                                    <div class="col-md-3">
+                                        <label for="OperatorId">OperatorId:</label>
+                                        <input type="text" class="form-control" id="OperatorId" name="OperatorId"
+                                            value="delightMMK" required>
+                                    </div>
 
-                                <label for="RequestDateTime">RequestDateTime:</label>
-                                <input type="text" id="RequestDateTime" name="RequestDateTime"
-                                    value="2024-11-11 12:12:12" required><br><br>
+                                    <div class="col-md-3">
+                                        <label for="RequestDateTime">RequestDateTime:</label>
+                                        <input type="text" class="form-control" id="RequestDateTime"
+                                            name="RequestDateTime" value="2024-11-11 12:12:12" required>
+                                    </div>
 
-                                <label for="Signature">Signature:</label>
-                                <input type="text" id="Signature" name="Signature"
-                                    value="b7acad4a4fb0de124cc64106ab6d0eea" required><br><br>
+                                    <div class="col-md-3">
+                                        <label for="Signature">Signature:</label>
+                                        <input type="text" class="form-control" id="Signature" name="Signature"
+                                            value="b7acad4a4fb0de124cc64106ab6d0eea" required>
+                                    </div>
 
-                                <label for="Date">Date:</label>
-                                <input type="text" id="Date" name="Date" value="2024-11-14T00:00:00Z"
-                                    required><br><br>
+                                    <div class="col-md-3">
+                                        <label for="Date">Date:</label>
+                                        <input type="text" class="form-control" id="Date" name="Date"
+                                            value="2024-11-14T00:00:00Z" required>
+                                    </div>
+                                </div>
 
-                                <button type="submit">Submit</button>
+                                <div class="form-row mt-4">
+                                    <div class="col text-center">
+                                        <button type="submit" class="btn btn-success">Submit</button>
+                                    </div>
+                                </div>
                             </form>
 
                             @if (session('response'))
-                                <h2>Response</h2>
-                                <pre>{{ json_encode(session('response'), JSON_PRETTY_PRINT) }}</pre>
+                                <div class="mt-4">
+                                    <h2>Response</h2>
+                                    <pre>{{ json_encode(session('response'), JSON_PRETTY_PRINT) }}</pre>
+                                </div>
                             @endif
-
                         </div>
-
-                        <!-- /.card-body -->
                     </div>
-                    <!-- /.card -->
                 </div>
             </div>
         </div>
